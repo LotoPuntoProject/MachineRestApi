05561b22b728e19f27bfd1cbdc74478eaf9018f7
raosoriom <raosoriom.lotopunto@gmail.com>
2020-07-21 19:45:35 -0500

    update 21-July

--- LotoPuntoApp/machine/views.py
+++ LotoPuntoApp/machine/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -16,18 +16,34 @@ def index(request):
     return HttpResponse("Loto Punto App")
 
 def login_request(user, password, terminalId):
-    url = ''
-    payload = {"user": user, "password": password, "terminalId": terminalId}
-    ans = requests.post(url, data=payload)
+    url = 'http://localhost:3000/api/v1/users/login'
+    user = 'noreplycincop@gmail.com'
+    password = 'XQW8UbVDwZ'
+    payload = {"email": user, "password": password, "terminalId": terminalId}
+    return requests.post(url, json=payload)
+
 
 # Create your views here.
 @api_view(['GET'])
-def autologin(request):
+def autologin(request, ):
     if request.method == 'GET':
-        pass
+        try:
+            queryset = LotoMachine.objects.get(pk=1)
+        except LotoMachine.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # user = get_object_or_404(queryset, pk=pk)
+        serializer = LotoMachineSerializer(queryset)
+        data = serializer.data
+        ans  = login_request(data['user'], data['password'], data['terminalId'])
+        status = ans.status_code
+        print(f"Respuesta del request: {ans.text} status: {status}")
+        # data = data.__dict__
+        print(f"data: {data['user']} type: {type(data)}")
+        return JsonResponse(serializer.data, safe=False)
         # snippets = Snippet.objects.all()
         # serializer = SnippetSerializer(snippets, many=True)
-        return Response("hello")
+        # return Response("hello")
 
     elif request.method == 'POST':
         pass
